cd23401b2b49226aeaab635f602b2fc4d420d5b3
Abhishek Survase <abhishek.survase2000@gmail.com>
2024-04-03 01:43:30 +0530

    Added NV-Chad Heirline and installed LSP: lua_ls

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.pack.java" },
   -- import/override with your plugins folder
 }
